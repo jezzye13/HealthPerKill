af5a7a6b779f5db240f3e72b823ed054cb1b78fe
Jessey Cool <jd.jessey@gmail.com>
2014-11-09 17:10:00 +0100

    Update Version 1.3
    - Removed set_full_health out of plugin

--- src/tk/wisdomunit/hpk/HealthPerKill.java
+++ src/tk/wisdomunit/hpk/HealthPerKill.java
@@ -8,12 +8,10 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class HealthPerKill extends JavaPlugin implements Listener {
-	
-	public static final String PATH_FULL_HEALTH_BOOLEAN = "set_full_health";
+
 	public static final String PATH_NUMBER_HEALTH_DOUBLE = "set_health";
 	public static final String PATH_SOUND_STRING = "set_sound";
-	
-	private boolean full_health;
+
 	private double number_health;
 	private String sound;
 
@@ -22,37 +20,30 @@ public class HealthPerKill extends JavaPlugin implements Listener {
 		getServer().getPluginManager().registerEvents(this, this);
 
 		saveDefaultConfig();
-		
+
 		try {
-			full_health = getConfig().getBoolean(PATH_FULL_HEALTH_BOOLEAN);
 			number_health = getConfig().getDouble(PATH_NUMBER_HEALTH_DOUBLE);
 			sound = getConfig().getString(PATH_SOUND_STRING);
 		} catch (NullPointerException ex) {
 			getLogger().warning("Can`t load config.yml! ;(");
-			getLogger().warning("Reinstall this plugin to fix (jar and folder)");
+			getLogger()
+					.warning("Reinstall this plugin to fix (jar and folder)");
 			getServer().getPluginManager().disablePlugin(this);
 		}
 	}
-	
-	@Override
-	public void onDisable() {
-	}
-	
+
 	@EventHandler
 	public void onKill(PlayerDeathEvent e) {
-		if(e.getEntity().getKiller() instanceof Player) {
+		if (e.getEntity().getKiller() instanceof Player) {
 			Player p = e.getEntity().getKiller();
-			
-			if(full_health) {
+
+			double out_come = p.getHealth() + number_health;
+			if (out_come > p.getMaxHealth()) {
 				p.setHealth(p.getMaxHealth());
 			} else {
-				double out_come = p.getHealth() + number_health;
-				if(out_come > p.getMaxHealth()) {
-					p.setHealth(p.getMaxHealth());
-				} else {
-					p.setHealth(out_come);
-				}
+				p.setHealth(out_come);
 			}
+
 			p.playSound(p.getLocation(), Sound.valueOf(sound), 1, 10);
 		}
 	}
